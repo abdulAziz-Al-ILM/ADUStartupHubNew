ac641e7200b859e12447f288f77428bebb28cafb
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-04-05 14:29:53 +0500

    Update otp.js

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,11 +1,21 @@
 /**
- * 4 xonali tasodifiy OTP kod generatori
- * @returns {string} Masalan: "4829"
+ * 4 xonali tasodifiy OTP kod generatori (Talabalar uchun)
  */
 const generateOTP = () => {
-  // 1000 dan 9999 gacha bo'lgan tasodifiy son
   const otp = Math.floor(1000 + Math.random() * 9000);
   return otp.toString();
 };
 
-module.exports = { generateOTP };
+/**
+ * 20 xonali juda murakkab kod generatori (Admin xavfsizligi uchun)
+ */
+const generateComplexOTP = (length = 20) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
+module.exports = { generateOTP, generateComplexOTP };
