748ac8c2dd2b43c91d0c5c0d69447cc7cfdfd1a9
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-04-05 09:21:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./src/routes/authRoutes');
+const userRoutes = require('./src/routes/userRoutes');       // Yangi qo'shildi
 const projectRoutes = require('./src/routes/projectRoutes');
-const resumeRoutes = require('./src/routes/resumeRoutes');   // Yangi
-const requestRoutes = require('./src/routes/requestRoutes'); // Yangi
+const resumeRoutes = require('./src/routes/resumeRoutes');   
+const requestRoutes = require('./src/routes/requestRoutes'); 
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // === MARSHRUTLAR (API ROUTES) ===
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);         // Profil uchun yo'nalish
 app.use('/api/projects', projectRoutes);
 app.use('/api/resumes', resumeRoutes);
 app.use('/api/requests', requestRoutes);
