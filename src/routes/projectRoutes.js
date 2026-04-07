56239e61072ea53b001e802934b0389620b65f9d
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-04-07 12:43:28 +0500

    Update projectRoutes.js

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
-const { verifyToken } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 // Barcha loyihalarni ko'rish va qidirish
-router.get('/', verifyToken, projectController.getAllProjects);
+router.get('/', protect, projectController.getAllProjects);
 
 // Yangi loyiha yaratish
-router.post('/', verifyToken, projectController.createProject);
+router.post('/', protect, projectController.createProject);
 
 // Loyiha holatini o'zgartirish (AI sabablari bilan)
-router.put('/:id/status', verifyToken, projectController.updateProjectStatus);
+router.put('/:id/status', protect, projectController.updateProjectStatus);
 
 module.exports = router;
