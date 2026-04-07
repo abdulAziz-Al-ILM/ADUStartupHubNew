9adfd0bf80e9317f8cf4230fb019a7899c418e74
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2026-04-07 12:34:53 +0500

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 
-// Profilni yuklash
 router.get('/me', verifyToken, userController.getProfile);
-
-// Profilni (Rezyumeni) tahrirlash
 router.put('/profile', verifyToken, userController.updateProfile);
 
 module.exports = router;
